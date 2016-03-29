23775389bbd506d033e6d411c8becd2c5157ac3b
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-03-29 09:33:27 +0200

    Update test.search.js

--- test/test.search.js
+++ test/test.search.js
@@ -8,42 +8,42 @@ describe('search', () => {
   }
 
   it('should search in an undefined array', () => {
-    var result = search(undefined, undefined);
+    const result = search(undefined, undefined);
     expect(result).to.be.undefined;
   });
 
   it('should search in an empty array', () => {
-    var result = search(undefined, []);
+    const result = search(undefined, []);
     expect(result).to.be.undefined;
   });
 
   it('should not found undefined in a single post array', () => {
-    var result = search(undefined, [1]);
+    const result = search(undefined, [1]);
     expect(result).to.be.undefined;
   });
 
   it('should not found a value in a single post array', () => {
-    var result = search(0, [1]);
+    const result = search(0, [1]);
     expect(result).to.be.undefined;
   });
 
   it('should found in a single post array', () => {
-    var result = search(1, [1]);
+    const result = search(1, [1]);
     expect(result).to.be.equal(1);
   });
 
   it('should found in bottom bound of a large array', () => {
-    var result = search(0, largeArray);
+    const result = search(0, largeArray);
     expect(result).to.be.equal(0);
   });
 
   it('should found in up bound of a large array', () => {
-    var result = search(4999, largeArray);
+    const result = search(4999, largeArray);
     expect(result).to.be.equal(4999);
   });
 
   it('should found in one shot of a large array', () => {
-    var result = search(2500, largeArray);
+    const result = search(2500, largeArray);
     expect(result).to.be.equal(2500);
   });
 });
